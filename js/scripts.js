65649514006a747f31f35cc9556dcaf5f3726c2f
gabriel nwachukwu <gabrielcoder247@gmail.com>
2018-05-09 09:58:11 +0300

    add display code to date on my ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 function Todolist(item, date) {
-  this.items = "item";
-  this.dates = "date";
+  this.item = item;
+  this.date = date ;
 
 }
 $(document).ready(function() {
   $("form#new-to-do-list").submit(function(event) {
     event.preventDefault();
-    var newItem = $("#new-item").val();
-    var newDate = $("#new-date").val();
+    var newItem = $("input#new-item").val();
+    var newDate = $("input#new-date").val();
     var newToDoList = new Todolist(newItem, newDate);
 
-    $("ul#things-to-do").append("<li><span>" + newToDoList.items + "</li></span>");
-    $("#new-item").val();
-    $("#new-date").val();
+    $("ul#things-to-do").append("<li><span>" + newToDoList.item + "</li></span>");
+    $("ul#things-to-do").append("<li><span>" + newToDoList.date + "</li></span>");
+    $("input#new-item").val('');
+    $("input#new-date").val('');
   });
 });
